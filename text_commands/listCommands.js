6bfec407190433c0d2144e1810ba601706e8e9ec
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-02-23 07:39:29 +0200

    Fix listCommands
    It was buggy, fixed now.

--- text_commands/listCommands.js
+++ text_commands/listCommands.js
@@ -1,9 +1,9 @@
-const commands = require("./commands.json");
+const commandData = require("./commands.json");
 
 module.exports = {
 	async execute(message, regexResults) {
-		let res = "You can ask me the following things:\n";
-		for (const command of commands) {
+		let res = "You may adress be my \"One\", \"Lady One\", \"Oh Glorious One\", \"Dear One\" and such other names.\nThen, adressing me, you can ask me the following things:\n";
+		for (const command of commandData.commands) {
 			res += `"${command.help}", ${command.description}\n`;
 		}
 		return { text: res };
