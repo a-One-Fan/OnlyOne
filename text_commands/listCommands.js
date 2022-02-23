6c00c61f5d6f404865f83315351169995d1305c1
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-02-23 08:12:06 +0200

    Add the ability to hide commands, and a hidden command

--- text_commands/listCommands.js
+++ text_commands/listCommands.js
@@ -4,7 +4,7 @@ module.exports = {
 	async execute(message, regexResults) {
 		let res = "You may adress be my \"One\", \"Lady One\", \"Oh Glorious One\", \"Dear One\" and such other names.\nThen, adressing me, you can ask me the following things:\n";
 		for (const command of commandData.commands) {
-			res += `"${command.help}", ${command.description}\n`;
+			if (!command.hidden) res += `"${command.help}", ${command.description}\n`;
 		}
 		return { text: res };
 	},
