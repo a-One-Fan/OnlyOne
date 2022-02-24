ae31e0736b2cd9365056726fdf254f86ec464dc0
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-02-24 17:06:02 +0200

    Add test render command

---
+++ text_commands/renderMonkeys.js
@@ -0,0 +1,11 @@
+const { downloadImage, renderBlend } = require("../extras/image_manip");
+const { MessageAttachment } = require("discord.js");
+
+module.exports = {
+	async execute(message, regexResults) {
+		await downloadImage(regexResults[1]);
+		await renderBlend("./extras/monkeys.blend");
+		const _file = new MessageAttachment("../extras/render0000.png");
+		return { text: "Here's your render.", files: [_file] };
+	},
+};
