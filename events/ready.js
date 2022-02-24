6fffc1853a1969a325d6801f7c80d0078ca5affc
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-02-24 11:14:13 +0200

    Initial Blender/Ffmpeg checks and config
    The bot will get spooked if it can't open those files from the config.

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,23 @@
+const { access } = require("fs");
+const { ffmpegLocation, blenderLocation } = require("../config.json");
+
 module.exports = {
 	name: "ready",
 	once: true,
 	async execute(client) {
-		await client.db.sync();
+		const db = await client.db.sync();
+		console.log(`Loaded db "${db}"`);
+
+		access(ffmpegLocation, (err) => {
+			if (err) console.log(`Error when trying to open Ffmpeg exe at "${ffmpegLocation}":\n${err}`);
+			else console.log("Ffmpeg exe can be opened successfully.");
+		});
+
+		access(blenderLocation, (err) => {
+			if (err) console.log(`Error when trying to open Blender exe at "${blenderLocation}":\n${err}`);
+			else console.log("Blender exe can be opened successfully.");
+		});
+
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
