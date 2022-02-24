e798f49e8e1c043212a106acc8ff3a14d41e752e
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-02-24 20:03:43 +0200

    Add initial ffmpeg support

--- extras/image_manip.js
+++ extras/image_manip.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const https = require("https");
 const { execFile } = require("child_process");
-const { blenderLocation, permittedUrls, downloadFilepath } = require("../config.json");
+const { blenderLocation, ffmpegLocation, permittedUrls, downloadFilepath } = require("../config.json");
 
 module.exports = {
 	downloadImage(url, filepath = downloadFilepath) {
@@ -26,6 +26,17 @@ module.exports = {
 	renderBlend(filepath, extension = "png", outpath = "//render", pythonics = "pass") {
 		return new Promise((resolve, reject) => {
 			const cp = execFile(blenderLocation, ["-b", filepath, "--python-expr", pythonics, "-f", "0", "-F", extension, "-o", outpath + "." + extension], (error, stdout, stderr) => {
+				if (error) {
+					reject(`Error when rendering!\nError:\n${error}\nstdout:\n${stdout}\nstderr:\n${stderr}`);
+				}
+			});
+			cp.on("error", (err) => reject(err))
+				.once("close", (code) => resolve(code));
+		});
+	},
+	doFfmpeg(args) {
+		return new Promise((resolve, reject) => {
+			const cp = execFile(ffmpegLocation, args, (error, stdout, stderr) => {
 				if (error) {
 					reject(`Error when rendering!\nError:\n${error}\nstdout:\n${stdout}\nstderr:\n${stderr}`);
 				}
