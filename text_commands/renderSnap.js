528e5d22a9f192f6f1084b3ea0d787d32076dc80
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-02-25 14:49:30 +0200

    Added size and distance params to snap, and better responses

--- text_commands/renderSnap.js
+++ text_commands/renderSnap.js
@@ -3,7 +3,7 @@ const { MessageAttachment } = require("discord.js");
 
 module.exports = {
 	async execute(message, regexResults) {
-		const [ impath, extension ] = await downloadImage(regexResults[5]);
+		const [ impath, extension ] = await downloadImage(regexResults[7]);
 		console.log("impath, extension:", impath, extension);
 		let resolution = await getResolution(impath + "." + extension);
 		let vfscale = [];
@@ -32,6 +32,19 @@ module.exports = {
 			shape = "Tile";
 		}
 
+		let distance = 3.0;
+		if (regexResults[5]) {
+			distance = parseFloat(regexResults[5]);
+			distance = clamp(1, 25, distance);
+		}
+		distance = distance * 0.01;
+
+		let size = 1.0;
+		if (regexResults[6]) {
+			size = parseFloat(regexResults[6]);
+			size = clamp(0.1, 20, size);
+		}
+
 		const pythonics = `
 import bpy
 bpy.data.scenes["Scene"].render.resolution_x = ${newHeight}
@@ -42,9 +55,11 @@ bpy.data.objects["Image"].select_set(True)
 bpy.ops.transform.resize(value=(${aspectRatio}, 1, 1), constraint_axis=(True, False, False))
 bpy.ops.object.transform_apply(location=False, rotation=False, scale=True)
 bpy.data.node_groups["Tile fadeout"].nodes["Object Info"].inputs[0].default_value = bpy.data.objects['${shape}']
+bpy.data.node_groups["Tile fadeout"].nodes["Distribution distance"].outputs[0].default_value = ${distance}
+bpy.data.node_groups["Tile fadeout"].nodes["Size modifier"].outputs[0].default_value = ${size}
 `;
 		await renderBlend("extras/snapped.blend", ["-a"], pythonics);
 		const _file = new MessageAttachment("./tmp/snaprender.mp4");
-		return { text: "Here's your render.", files: [_file] };
+		return { text: ["Bye bye.", "It's no more.", "There it goes...", "Watch it vanish."][(Math.floor(Math.random() * 4))], files: [_file] };
 	},
 };
