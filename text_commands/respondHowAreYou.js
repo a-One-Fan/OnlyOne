b3cc7afb0b8e3e1374e96685f577eb6b1016555f
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-02-25 15:09:16 +0200

    Add "I'm ..." and "How are you..." commands
    To make the bot appear more talkative

---
+++ text_commands/respondHowAreYou.js
@@ -0,0 +1,9 @@
+module.exports = {
+	async execute(message, regexResults) {
+		const responses = ["I'm doing fine, thanks for asking.", "I'm fine.", "I'm feeling slightly happier right now. I haven't felt like that in a while.",
+			"I'm not feeling particularly great, sorry.", "I'm peeved.", "I'm feeling uneasy right now.",
+			"I'm glad you're concerned about me. I'm alright."];
+
+		return { text: responses[Math.floor(Math.random() * responses.length)] };
+	},
+};
---
+++ text_commands/respondIm.js
@@ -0,0 +1,10 @@
+module.exports = {
+	async execute(message, regexResults) {
+		const responses = ["I'm glad that you are.", "Alright.", "Good for you.",
+			"That doesn't sound good.", "I hope you're not in the future.", "Too bad...", "I'm busy right now.",
+			"Please ask me another time.", "That you are.", `Are you hoping for a "Hello [${regexResults[1].substring(0, 8)}...], I'm One!" ?\nYou're not going to get that from me.`,
+			"I regret reading your message.", "I'm happy I read that." ];
+
+		return { text: responses[Math.floor(Math.random() * responses.length)] };
+	},
+};
