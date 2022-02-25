6b49538aee2826dff1f9b36733a66fe1584ad6dc
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-02-25 08:21:08 +0200

    Add initial "thanos" snap command
    needs pythonics and resolution getting

---
+++ text_commands/renderSnap.js
@@ -0,0 +1,14 @@
+const { downloadImage, renderBlend, doFfmpeg } = require("../extras/image_manip");
+const { MessageAttachment } = require("discord.js");
+
+module.exports = {
+	async execute(message, regexResults) {
+		const [ impath, extension ] = await downloadImage(regexResults[1]);
+		console.log("impath, extension:", impath, extension);
+		await doFfmpeg(["-stream_loop", "-1", "-r", "30", "-i", impath + "." + extension, "-frames:v", "240", "-t", "8", "-y", "./tmp/snapvid.mp4"]);
+
+		await renderBlend("./extras/snapped.blend");
+		const _file = new MessageAttachment("./tmp/snaprender.mp4");
+		return { text: "Here's your render.", files: [_file] };
+	},
+};
