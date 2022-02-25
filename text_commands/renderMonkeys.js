74a4da0eddbd936acc6c3b86f3686567c31974e6
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-02-25 19:39:27 +0200

    Update renderMonkeys
    It was broken by the change to renderBlend, fixed now.

--- text_commands/renderMonkeys.js
+++ text_commands/renderMonkeys.js
@@ -5,9 +5,9 @@ module.exports = {
 	async execute(message, regexResults) {
 		const [ impath, extension ] = await downloadImage(regexResults[1]);
 		console.log("impath, extension:", impath, extension);
-		await doFfmpeg(["-i", impath + "." + extension, "-frames:v", "1", "-y", impath + ".png"]);
-		await renderBlend("./extras/monkeys.blend");
-		const _file = new MessageAttachment("./tmp/render0000.png");
+		await doFfmpeg(["-i", impath + "." + extension, "-frames:v", "1", "-y", "./tmp/monkeyStuff.png"]);
+		await renderBlend("./extras/monkeys.blend", ["-f", "0"]);
+		const _file = new MessageAttachment("./tmp/monkeyPic0000.png");
 		return { text: "Here's your render.", files: [_file] };
 	},
 };
