6a625b0d3f30b9e82f264cdcda596a6cb0874320
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-02-25 20:10:17 +0200

    Pagination! For listing commands.
    pagination, such a funny word
    Also fixes incorrect regex in help reactTypes matching accidentally

--- text_commands/listCommands.js
+++ text_commands/listCommands.js
@@ -1,11 +1,28 @@
 const commandData = require("./commands.json");
+const { clamp } = require("../extras/image_manip.js");
+
+const commandsPerPage = 8;
 
 module.exports = {
 	async execute(message, regexResults) {
-		let res = "You may adress be my \"One\", \"Lady One\", \"Oh Glorious One\", \"Dear One\" and such other names.\nThen, adressing me, you can ask me the following things:\n";
-		for (const command of commandData.commands) {
+		let page = 1;
+		if (regexResults[1]) page = parseInt(regexResults[1]);
+		const fakePage = page;
+		page = clamp(1, Math.ceil(commandData.commands.length / commandsPerPage), page);
+		const commandsStart = clamp(0, commandData.commands.length - 1, (page - 1) * commandsPerPage);
+		const commandsEnd = clamp(0, commandData.commands.length, page * commandsPerPage);
+
+		let res = "";
+		if (page == 1) res = "You may adress be my \"One\", \"Lady One\", \"Oh Glorious One\", \"Dear One\" and such other names.\nThen, adressing me, you can ask me the following things:\n";
+
+		for (let i = commandsStart; i < commandsEnd; i++) {
+			const command = commandData.commands[i];
 			if (!command.hidden) res += `"${command.help}", ${command.description}\n`;
 		}
+
+		const fakePageStr = (page == fakePage) ? `${page}` : `"${fakePage}"`;
+		res += `\n(Page ${fakePageStr}/${Math.ceil(commandData.commands.length / commandsPerPage)})`;
+
 		return { text: res };
 	},
 };
