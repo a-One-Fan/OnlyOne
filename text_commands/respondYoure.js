e51f1c747db4142d4c1a435d2ef8f1ca082c895c
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-02-25 19:39:48 +0200

    Add "You're ..." interaction command

---
+++ text_commands/respondYoure.js
@@ -0,0 +1,8 @@
+module.exports = {
+	async execute(message, regexResults) {
+		const responses = ["Thanks.", "Thanks.", "OK.", "I don't think so.", "I guess I am.", "I guess so.", "If you say so.", "Oh really?", "I'd rather not be that, then.",
+			"No, I'm not.", `Are *you* "${regexResults[1]}"?`, "You can flatter me all you want but don't expect something in return."];
+
+		return { text: responses[Math.floor(Math.random() * responses.length)] };
+	},
+};
