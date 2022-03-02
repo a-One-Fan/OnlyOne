c95f806bb042d086aaae6218a383078cb117fa74
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-02 21:24:23 +0200

    Add support for tidier regex-based parameters for commands

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -54,8 +54,12 @@ module.exports = {
 				const reactRes = RegExp(command.regex, command.regexParams).exec(split[1]);
 				if (reactRes) {
 					try {
+						const extraRes = [];
+						for (const extraRegex of command.extraRegex) {
+							extraRes.push(RegExp(extraRegex, command.regexParams).exec(split[1]));
+						}
 						const func = require("../text_commands/" + command.name + ".js");
-						commandRes = await func.execute(message, reactRes);
+						commandRes = await func.execute(message, reactRes, extraRes);
 					} catch (error) {
 						commandError = true;
 						// TODO: DM the error to me? :)
