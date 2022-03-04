71659ddd3643d5d78dab07743a7a0dd7ee3cde34
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-04 14:30:24 +0200

    Add more whatIs responses

--- text_commands/respondWhatIs.js
+++ text_commands/respondWhatIs.js
@@ -4,9 +4,10 @@ module.exports = {
 	async execute(message, regexResults) {
 		let responses = [];
 		if (regexResults[1] == "is" || regexResults[1] == "'s" || regexResults[1] == "s") {
-			responses = ["1.", "11.", "10.", "100.", "111", "101", "A dog.", "A cat.", "A dragon.", "It's pointless.", "Crazy."];
+			responses = ["1.", "11.", "10.", "100.", "111.", "101.", "A dog.", "A cat.", "A dragon.", "It's pointless.", "It's crazy.", "It's stupid.", 
+				"It's mildly annoying.", "It's something I don't really care about."];
 		} else {
-			responses = ["Please give me plural whatIs responses :("];
+			responses = ["They're stupid.", "They're somewhere they should not be."];
 		}
 
 		return { text: pickRandom(responses) };
