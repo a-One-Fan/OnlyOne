102a112eb0618a6bd535ebd7f8bffebe94578522
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-04 13:18:52 +0200

    Add another respond command

---
+++ text_commands/respondWhatAre.js
@@ -0,0 +1,16 @@
+module.exports = {
+	async execute(message, regexResults) {
+		let responses = [];
+		if (regexResults[1] == "is") {
+			responses = ["I'm doing fine, thanks for asking.", "I'm fine.", "I'm feeling slightly happier right now. I haven't felt like that in a while.",
+				"I'm not feeling particularly great, sorry.", "I'm peeved.", "I'm feeling uneasy right now.",
+				"I'm glad you're concerned about me. I'm alright."];
+		} else {
+			responses = ["I'm doing fine, thanks for asking.", "I'm fine.", "I'm feeling slightly happier right now. I haven't felt like that in a while.",
+				"I'm not feeling particularly great, sorry.", "I'm peeved.", "I'm feeling uneasy right now.",
+				"I'm glad you're concerned about me. I'm alright."];
+		}
+
+		return { text: responses[Math.floor(Math.random() * responses.length)] };
+	},
+};
