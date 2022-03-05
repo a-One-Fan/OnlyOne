4bae726c8bc20d221177972972b08cd20c5d3166
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-05 19:49:26 +0200

    Add operators and update the calculate command info

--- extras/math_stuff.js
+++ extras/math_stuff.js
@@ -30,7 +30,7 @@ module.exports = {
 		km: { value: 1000, type: "distance", subtype: "metric distance", names: ["km", "kilometer", "kilometre", "kilometers", "kilometres"] },
 		in: { value: 0.0254, type: "distance", subtype: "imperial distance", names: ["in", "inch", "inches"] },
 		ft: { value: 0.3048, type: "distance", subtype: "imperial distance", names: ["ft", "foot", "feet", "footfetish"] },
-		
+		mil: { value: 1609.344, type: "distance", subtype: "imperial distance", names: ["mil", "mile", "miles"] },
 
 		c: { type: "heat", subtype: "metric heat", names: ["c", "celsius", "centigrade"] },
 		f: { type: "heat", subtype: "imperial heat", names: ["f", "farenheit"] },
@@ -49,6 +49,27 @@ module.exports = {
 		deg: { value: 180 / Math.PI, type: "angle", names: ["d", "deg", "degrees"] },
 
 		untype: { type: "untype", names: ["un", "untype", "typeless"] },
+	},
+	// TODO: how tf do you refer to default functions like + as a function? To assign to a variable?
+	// TODO: trig functions
+	operators: {
+		plus: { args: 2, names: ["+", "plus"], op: (val1, val2) => { return val1 + val2; } },
+		minus: { args: 2, names: ["-", "minus"], op: (val1, val2) => { return val1 - val2; } },
+		times: { args: 2, names: ["*", "times"], op: (val1, val2) => { return val1 * val2; } },
+		divide: { args: 2, names: ["/", "divide", "divided", "divideby", "dividedby"], op: (val1, val2) => { return val1 / val2; } },
+		power: { args: 2, names: ["^", "**", "pow", "power"], op: Math.pow },
+		log: { args: 2, names: ["log", "logarithm"], op: (val1, val2) => { return Math.log(val1) / Math.log(val2); } },
+		or: { args: 2, names: ["||", "or"], op: (val1, val2) => { return val1 || val2; } },
+		bitor: { args: 2, names: ["|", "bitor", "bitwiseor"], op: (val1, val2) => { return val1 | val2; } },
+		and: { args: 2, names: ["&&", "and"], op: (val1, val2) => { return val1 && val2; } },
+		bitand: { args: 2, names: ["&", "bitand", "bitwiseand"], op: (val1, val2) => { return val1 & val2; } },
+		mod: { args: 2, names: ["%", "mod", "fmod"], op: (val1, val2) => { return val1 % val2; } },
+
+		abs: { args: 1, names: ["abs", "fabs"], op: Math.abs },
+		not: { args: 1, names: ["~", "not"], op: (val) => { return !val; } },
+		floor: { args: 1, names: ["floor"], op: Math.floor },
+		ceil: { args: 1, names: ["ceil", "ceiling"], op: Math.ceil },
+		fract: { args: 1, names: ["fract", "fraction", "fractpart", "fractionpart"], op: Math.fract },
 	},
 	converters: {
 		"metric heat": { "imperial heat": (val, from, to) => {return (val * 1.8) + 32; } },
