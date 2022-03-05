85c43c43da843ad961f6e4e6bc53e7392debb624
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-05 17:49:29 +0200

    Add a bit more whatIs responses

--- text_commands/respondWhatIs.js
+++ text_commands/respondWhatIs.js
@@ -4,7 +4,7 @@ module.exports = {
 	async execute(message, regexResults) {
 		let responses = [];
 		if (regexResults[1] == "is" || regexResults[1] == "'s" || regexResults[1] == "s") {
-			responses = ["1.", "11.", "10.", "100.", "111.", "101.", "A dog.", "A cat.", "A dragon.", "It's pointless.", "It's crazy.", "It's stupid.", 
+			responses = ["1.", "11.", "10.", "100.", "111.", "101.", "110.", "A dog.", "A cat.", "A dragon.", "My dragon friend.", "It's pointless.", "It's crazy.", "It's stupid.",
 				"It's mildly annoying.", "It's something I don't really care about."];
 		} else {
 			responses = ["They're stupid.", "They're somewhere they should not be."];
