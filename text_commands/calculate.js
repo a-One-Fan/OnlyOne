7f43f8c3212a73a8f1c62561f3f4710c698fe750
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-05 21:39:49 +0200

    More detailed printing for a >1 stack

--- text_commands/calculate.js
+++ text_commands/calculate.js
@@ -19,7 +19,7 @@ module.exports = {
 
 		const stack = [];
 
-		for (let i = 0; i < polish.length(); i++) {
+		for (let i = 0; i < polish.length; i++) {
 			if (polish[i].isNumber) {
 				stack.push(polish[i]);
 				// TODO: do this better?
@@ -31,7 +31,14 @@ module.exports = {
 				stack.push(polish[i].op(stack.pop(), stack.pop()));
 			}
 		}
-		if (stack.length > 1) return { text: "You didn't use enough operators." };
+		if (stack.length > 1) {
+			let remains = "";
+			for (let i = 0; i < (stack.length - 1); i++) {
+				remains += `${stack[i].value} ${stack[i].unit.names[stack[i].unit.names.length - 1]}, `;
+			}
+			remains += `${stack[stack.length - 1].value} ${stack[stack.length - 1].unit.names[stack[stack.length - 1].unit.names.length - 1]}\n`;
+			return { text: `You didn't use enough operators. What remains is:\n${remains}` };
+		}
 		if (stack.length == 0) return { text: "How did you leave an empty stack?!" };
 
 		return { text: `${stack[0].value}${stack[0].unit == units.untyped ? "" : ` ${stack[0].unit.names[stack[0].unit.names.length - 1]}.` }` };
