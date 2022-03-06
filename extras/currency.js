3500aed83e1c5a7467fd843bd2eecac8bc0e8c80
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-06 22:53:37 +0200

    Fix currency updating
    god why is https.get such a pain

--- extras/currency.js
+++ extras/currency.js
@@ -1,24 +1,33 @@
-const { https } = require("https");
+const https = require("https");
 const { exchangerateApiKey, baseCurrency, currencyUpdateInterval } = require("../config.json");
-const { fs } = require("fs");
+const fs = require("fs");
+const { stringify } = require("querystring");
 
 module.exports = {
-	forceUpdateCurrencies() {
-		https.fetch(`https://v6.exchangerate-api.com/v6/${exchangerateApiKey}/latest/${baseCurrency}`)
-			.then(res => {
-				const jsonedRes = JSON.parse(res);
-				const currencyData = require("./extras/currencies.json");
+	async forceUpdateCurrencies() {
+		let res = [];
+		res = await new Promise((resolve, reject) => {
+			https.get(`https://v6.exchangerate-api.com/v6/${exchangerateApiKey}/latest/${baseCurrency}`, (msg) => {
+				msg.setEncoding("utf-8");
+				let str = "";
+				msg.on("data", (data) => { str += data; });
+				msg.on("end", () => {resolve(str);});
 
-				if (jsonedRes.result != "success") throw Error(`Failed to get currencies: ${jsonedRes.result}`);
+			}).on("error", reject);
+		});
 
-				currencyData.currencies = jsonedRes.conversion_rates;
-				currencyData.lastUpdated = Date(jsonedRes.time_last_update_unix * 1000).toJSON();
+		const jsonedRes = JSON.parse(res);
+		const currencyData = require("./currencies.json");
 
-				fs.writeFileSync("./extras/currencies.json", currencyData.stringify());
-			});
+		if (jsonedRes.result != "success") throw Error(`Failed to get currencies: ${jsonedRes.result}`);
+
+		currencyData.currencies = jsonedRes.conversion_rates;
+		currencyData.lastUpdated = (new Date(jsonedRes.time_last_update_unix * 1000)).toJSON();
+
+		fs.writeFileSync("./extras/currencies.json", JSON.stringify(currencyData, null, 2));
 	},
 	updateCurrencies() {
-		const { lastUpdated } = require("./extras/currencies.json");
+		const { lastUpdated } = require("./currencies.json");
 		const now = new Date();
 		const lastUpdatedDate = new Date(lastUpdated);
 		if ((now - lastUpdatedDate) < currencyUpdateInterval) return now - lastUpdatedDate;
