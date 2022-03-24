e7df7fe5a28efa77df4e4a589618a6669b475165
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-24 12:18:08 +0200

    Add info for otherOne count

--- text_commands/printAuthor.js
+++ text_commands/printAuthor.js
@@ -10,7 +10,7 @@ module.exports = {
 			reactTypeText = "I will " + reactTypeText;
 		}
 		let res = `You have said my name a total of ${row.lowerOne + row.upperOne} times (${row.upperOne} capitalized ${row.reputation > 5.0 ? "" : "correctly"} and ${row.lowerOne} not),\n` +
-		`you've said the digit "1" ${row.digitOne} times. Your reputation is ${row.reputation}${row.reputation > 5.0 ? "!" : "."}\n` +
+		`you've said the digit "1" ${row.digitOne} times and you've mentioned me with other names ${row.otherOne} times. Your reputation is ${row.reputation}${row.reputation > 5.0 ? "!" : "."}\n` +
 		`${reactTypeText} (type ${row.reactType}), with a ${row.reactChance * 100.0}% chance (${row.reactChance})${row.reputation > 5.0 ? "!" : "."}\n`;
 		if (row.addendum) res += `Addendum: ${row.addendum}\n`;
 
