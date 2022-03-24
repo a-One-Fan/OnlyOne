569178760f2ce49b8d471e33e710e271959b8e17
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-24 16:30:52 +0200

    More sensible listErrorTypes text

--- text_commands/listErrorTypes.js
+++ text_commands/listErrorTypes.js
@@ -3,7 +3,7 @@ const { errorTypes } = require("../configurables/error_message.js");
 module.exports = {
 	// TODO: streamline this code?
 	execute(message, regexResults) {
-		let res = "Whenever an error occurs, I can do the following:\n";
+		let res = "Whenever an error occurs, I can send the following:\n";
 		for (const [i, errorType] of errorTypes.entries()) {
 			const nameCap = errorType.name[0].toUpperCase() + errorType.name.substring(1);
 			res += `${i}: ${nameCap}.\n`;
