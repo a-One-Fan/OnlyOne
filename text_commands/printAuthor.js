49ba42cac68b8c4a2e9e04f7d27a3ed5b3cd18d9
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-24 16:04:56 +0200

    Print error and parse types in printAuthor

--- text_commands/printAuthor.js
+++ text_commands/printAuthor.js
@@ -1,18 +1,22 @@
+const reacts = require("../configurables/message_reacts.js");
+const parseTypes = require("../configurables/valid_message.js");
+const errorTypes = require("../configurables/error_message.js");
+
 module.exports = {
 	async execute(message, regexResults) {
 		const row = await message.client.db.findOne({ where: { userID: message.author.id } });
-		let reactTypeText = "";
-		const reacts = require("../configurables/message_reacts.js");
-		if (row.reactType < 0 || row.reactType >= reacts.reactTypes.length)	{
-			reactTypeText = reacts.unknownReact;
-		} else {
-			reactTypeText = reacts.reactTypes[row.reactType].name;
-			reactTypeText = "I will " + reactTypeText;
-		}
-		let res = `You have said my name a total of ${row.lowerOne + row.upperOne} times (${row.upperOne} capitalized ${row.reputation > 5.0 ? "" : "correctly"} and ${row.lowerOne} not),\n` +
+
+		const reactTypeText = "I will " + reacts.reactTypes[row.reactType].name;
+
+		const parseTypeText = parseTypes[row.parseType].name;
+
+		const errorTypeText = errorTypes[row.errorType].name;
+
+		const res = `You have said my name a total of ${row.lowerOne + row.upperOne} times (${row.upperOne} capitalized ${row.reputation > 5.0 ? "" : "correctly"} and ${row.lowerOne} not),\n` +
 		`you've said the digit "1" ${row.digitOne} times and you've mentioned me with other names ${row.otherOne} times. Your reputation is ${row.reputation}${row.reputation > 5.0 ? "!" : "."}\n` +
-		`${reactTypeText} (type ${row.reactType}), with a ${row.reactChance * 100.0}% chance (${row.reactChance})${row.reputation > 5.0 ? "!" : "."}\n`;
-		if (row.addendum) res += `Addendum: ${row.addendum}\n`;
+		`${reactTypeText} (type ${row.reactType}), with a ${row.reactChance * 100.0}% chance (${row.reactChance})${row.reputation > 5.0 ? "!" : "."}\n` +
+		`I will consider as a valid text command ${parseTypeText}. (type ${row.parseType})\n` +
+		`When an error occurs, I will respond with ${errorTypeText}. (type ${row.errorType})\n`;
 
 		return { text: res };
 	},
