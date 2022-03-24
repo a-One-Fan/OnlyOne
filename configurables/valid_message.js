f502ca17fceb5183c945d9723b9797dcb494d7f1
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-24 16:10:11 +0200

    Reword parseTypes' names

--- configurables/valid_message.js
+++ configurables/valid_message.js
@@ -2,22 +2,22 @@ const { validStart, validEnd } = require("../extras/text_recognition.js");
 
 module.exports = {
 	parseTypes: [
-		// The name should start with something that you could sensibly add "A valid text command will " to, and not end with punctuation.
-		{ name: "refer to me only at the start of your message",
+		// The name should start with something that you could sensibly add "I will recognize as a valid text command " to, and not end with punctuation.
+		{ name: "when you refer to me only at the start of your message",
 			testParse(text) {
 				const res = validStart(text);
 
 				return { valid: Boolean(res), culledText: res ? res[1] : "" };
 			},
 		},
-		{ name: "refer to me only at the end of your message",
+		{ name: "when you refer to me only at the end of your message",
 			testParse(text) {
 				const res = validEnd(text);
 
 				return { valid: Boolean(res), culledText: res ? res[1] : "" };
 			},
 		},
-		{ name: "refer to me at the start and/or end of your message",
+		{ name: "when you refer to me at the start and/or end of your message",
 			testParse(text) {
 				const startCut = validStart(text);
 				let bothCut = "";
@@ -32,13 +32,13 @@ module.exports = {
 				return { valid: Boolean(bothCut), culledText: bothCut };
 			},
 		},
-		{ name: "refer to me at the start and/or end of your message, or start with \".one\"",
+		{ name: "when you refer to me at the start and/or end of your message, or start with \".one\"",
 			testParse(text) {
 				if (text.substr(0, 4) == ".one") return { valid: true, culledText: text.substr(4) };
 				return module.exports.parseTypes[2].testParse(text);
 			},
 		},
-		{ name: "be any message, as well as ones that refer to me at the start/end or start with \".one\"",
+		{ name: "any message, as well as ones that refer to me at the start/end or start with \".one\"",
 			testParse(text) {
 				const tested = module.exports.parseTypes[3].testParse(text);
 				if (!tested.valid) return { valid: true, culledText: text };
