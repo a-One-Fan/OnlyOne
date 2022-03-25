44da1b95c6f62be434d7f2a6f587f76a8f42daf4
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-25 15:35:52 +0200

    Add option to reset react emote

--- text_commands/setAuthorReactEmote.js
+++ text_commands/setAuthorReactEmote.js
@@ -1,11 +1,17 @@
 module.exports = {
 	async execute(message, regexResults, extraRegex) {
+		if (regexResults[1]) {
+			await message.client.db.update({ customReact: null }, { where: { userID: message.author.id } });
+			return { text: "I've reset your react emote." };
+		}
+
 		try {
-			await message.react(regexResults[1]);
+			await message.react(regexResults[2]);
 		} catch (error) {
 			// Don't catch this and instead use the special error-catching code?
 			return { text: "I can't react using that emote, sorry." };
 		}
+
 		await message.client.db.update({ customReact: regexResults[1] }, { where: { userID: message.author.id } });
 		return { text: `I've set your custom react emote to "${regexResults[1]}".` };
 	},
