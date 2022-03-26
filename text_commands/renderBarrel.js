3096ef3dd1b5627170f09d8d49510aeb5f157a80
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-26 19:48:30 +0200

    Bugfixes for barrel roll

--- text_commands/renderBarrel.js
+++ text_commands/renderBarrel.js
@@ -4,13 +4,13 @@ const { MessageAttachment } = require("discord.js");
 module.exports = {
 	async execute(message, regexResults) {
 		let targetUser = message.client.user;
-		if (regexResults[3]) targetUser = message.user;
+		if (regexResults[1]) targetUser = message.author;
 		if (regexResults[2]) {
 			const found = message.client.users.cache.find(u => u.id == regexResults[2]);
 			if (!found) throw Error("Printable error: Sorry, I couldn't find a user by that ID.");
 			targetUser = found;
 		}
-		if (regexResults[1]) {
+		if (regexResults[3]) {
 			const lower = regexResults[1].toLowerCase();
 			const found = message.client.users.cache.find(u => u.username.toLowerCase().search(lower) != -1);
 			if (!found) throw Error("Printable error: Sorry, I couldn't find a user by that name.");
@@ -20,7 +20,7 @@ module.exports = {
 		console.log("impath, extension:", impath, extension);
 		await doFfmpeg(["-i", impath + "." + extension, "-y", "./tmp/barrelPicture.png"]);
 		await renderBlend("./extras/barrel.blend", ["-a"]);
-		await doFfmpeg(["-i", "barrelResult.mkv", "-lavfi", "\"[0:v]palettegen[pal]; [0:v][pal]paletteuse\"", "-y", "barrelRoll.gif"]);
+		await doFfmpeg(["-i", "./tmp/barrelResult.mkv", "-lavfi", "[0:v]palettegen[pal]; [0:v][pal]paletteuse", "-y", "./tmp/barrelRoll.gif"]);
 		const _file = new MessageAttachment("./tmp/barrelRoll.gif");
 		return { text: "Rolling...", files: [_file] };
 	},
