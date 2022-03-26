a25ad4cfcd0f269585df711e32c38a13a283ea1e
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-26 14:16:42 +0200

    Lint some whatIs responses

--- text_commands/respondWhatIs.js
+++ text_commands/respondWhatIs.js
@@ -5,7 +5,8 @@ module.exports = {
 		let responses = [];
 		if (regexResults[1] == "is" || regexResults[1] == "'s" || regexResults[1] == "s") {
 			responses = ["1.", "11.", "10.", "A dog.", "A cat.", "A dragon.", "My dragon friend.", "It's pointless.", "It's crazy.", "It's stupid.",
-				"It's mildly annoying.", "It's something I don't really care about.", "It's the worst I've had the displeasure of seeing.", "It's the best I've seen, so far.", "It's simply great.", "It's terrible.", "It's nice.", 
+				"It's mildly annoying.", "It's something I don't really care about.", "It's the worst I've had the displeasure of seeing.",
+				"It's the best I've seen, so far.", "It's simply great.", "It's terrible.", "It's nice.",
 				"It's disgusting and disfigured, and does not redeem itself for those qualities.", "It smells nice."];
 		} else {
 			responses = ["They're stupid.", "They're somewhere they should not be."];
