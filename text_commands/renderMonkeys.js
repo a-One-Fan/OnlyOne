2155b5c31a6817218134bbd71d15c13a91f5cdcc
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-30 20:03:13 +0300

    Log monkey render times

--- text_commands/renderMonkeys.js
+++ text_commands/renderMonkeys.js
@@ -5,10 +5,21 @@ const { MessageAttachment } = require("discord.js");
 module.exports = {
 	async execute(message, regexResults) {
 		const link = await getLinkFromText(regexResults[1], message);
+		console.log("Working on monkeys...");
+		let time = new Date();
+
 		const [ impath, extension ] = await downloadImage(link);
-		console.log("impath, extension:", impath, extension);
+		console.log(`Monkeys took ${(new Date() - time) / 1000.0}s to download input.`);
+		time = new Date();
+
 		await doFfmpeg(["-i", impath + "." + extension, "-frames:v", "1", "-y", "./tmp/monkeyStuff.png"]);
+		console.log(`Monkeys took ${(new Date() - time) / 1000.0}s to convert input.`);
+		time = new Date();
+
 		await renderBlend("./extras/monkeys.blend", ["-f", "0"]);
+		console.log(`Monkeys took ${(new Date() - time) / 1000.0}s to render.`);
+		time = new Date();
+
 		const _file = new MessageAttachment("./tmp/monkeyPic0000.png");
 		return { text: "Here's your render.", files: [_file] };
 	},
