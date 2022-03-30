a038eaabc27aa41a12fc92a8c5f2c20ab1e7b81e
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-03-30 19:58:23 +0300

    Log time taken of each step when rendering barrel

--- text_commands/renderBarrel.js
+++ text_commands/renderBarrel.js
@@ -5,10 +5,24 @@ const { MessageAttachment } = require("discord.js");
 module.exports = {
 	async execute(message, regexResults) {
 		const link = await getLinkFromText(regexResults[1], message);
+		console.log("Working on barrel...");
+		let time = new Date();
+
 		const [ impath, extension ] = await downloadImage(link);
-		await toGoodVideo(impath + "." + extension, 25, "barrelPicture");
+		console.log(`Barrel took ${(new Date() - time) / 1000.0}s to download input.`);
+		time = new Date();
+
+		await toGoodVideo(impath + "." + extension, 25, "barrelPicture", 2.5);
+		console.log(`Barrel took ${(new Date() - time) / 1000.0}s to convert input to video.`);
+		time = new Date();
+
 		await renderBlend("./extras/barrel.blend", ["-a"]);
+		console.log(`Barrel took ${(new Date() - time) / 1000.0}s to render.`);
+		time = new Date();
+
 		await doFfmpeg(["-i", "./tmp/barrelResult.mkv", "-lavfi", "[0:v]palettegen[pal]; [0:v][pal]paletteuse", "-y", "./tmp/barrelRoll.gif"]);
+		console.log(`Barrel took ${(new Date() - time) / 1000.0}s to convert output to gif.`);
+
 		const _file = new MessageAttachment("./tmp/barrelRoll.gif");
 		return { text: "Rolling...", files: [_file] };
 	},
