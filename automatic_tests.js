7fa707835296e1c25a2b66c33c7339ec31dc2fb4
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-04-03 00:50:49 +0300

    Add more tests and some improvements

--- automatic_tests.js
+++ automatic_tests.js
@@ -1,19 +1,37 @@
 const { executeCommand } = require("./text_commands/text_command_utils.js");
 
+// Some long string of stuff that you won't manually write.
+// If a test has this string in its out for one of its attributes, then it will consider as valid anything in that attribute.
+// This is to make testing for files doable.
+const ANY = "This represents the possibility for any input.";
+
 module.exports = {
-	// Some long string of stuff that you won't manually write.
-	// If a test has this string in its out for one of its attributes, then it will consider as valid anything in that attribute.
-	// This is to make testing for files doable.
-	any: "djas981hiubvdi12837bc",
-
 	text_command_basic_tests: [
+		{ parseType: 4, errorType: 4 },
 
-		{ parseType: 4, errorType: 4, category: "Hello" },
+		{ category: "Parsing" },
 		{ in: "One, hello!", out: { text: "https://imgur.com/J4bT846.png" } },
 		{ in: "One, Hello!", out: { text: "https://imgur.com/J4bT846.png" } },
 		{ in: ".onehello", out: { text: "https://imgur.com/J4bT846.png" } },
 		{ in: "One, hello, One!", out: { text: "https://imgur.com/J4bT846.png" } },
 
+		{ category: "Calculate" },
+		{ in: "One, calculate 2+3", out: { text: "5." } },
+		{ in: "One, calculate 2*3", out: { text: "6." } },
+		{ in: "One, calculate (6+7)*8", out: { text: "104." } },
+		{ in: "One, calculate floor ((sin 1) * 2)", out: { text: "1." } },
+		{ in: "One, calculate 1 m to cm", out: { text: "100 centimetres." } },
+
+		{ category: "Renders" },
+		{ in: "One, snap this: https://i.imgur.com/J4bT846.png", out: { text: ANY, files: ANY } },
+		{ in: "One, test render this: https://i.imgur.com/J4bT846.png", out: { text: "Here's your render.", files: ANY } },
+		{ in: "One, barrel roll https://i.imgur.com/J4bT846.png", out: { text: "Rolling...", files: ANY } },
+
+		{ category: "Image Macros" },
+		{ in: "One, hello!", out: { text: "https://imgur.com/J4bT846.png" } },
+		{ in: "One, goodbye!", out: { text: "https://imgur.com/J4bT846.png" } },
+		{ in: "One, shower!", out: { text: "https://imgur.com/dOujMWX.webm" } },
+		{ in: "One, dance!", out: { text: "https://imgur.com/KT7TTUk.webm" } },
 	],
 	// The call:
 	// message.client.db.findOne({ where: { userID: message.author.id } });
@@ -31,6 +49,10 @@ module.exports = {
 				return undefined;
 			} } },
 			author: { id: 1 },
+			reply(content) {
+				console.log("While testing, got a reply: ");
+				console.log(content);
+			},
 		};
 		let currentCategory = "";
 		for (const test of tests) {
@@ -62,7 +84,7 @@ module.exports = {
 
 				if (isEqual) {
 					for (const prop in test.out) {
-						if (test.out[prop] == module.exports.any) {
+						if ((test.out[prop] == ANY) && (res[prop])) {
 							continue;
 						}
 						if (test.out[prop] != res[prop]) {
@@ -77,15 +99,17 @@ module.exports = {
 			}
 		}
 
+		console.log("\n");
+
 		if (!unsuccessful.length) {
 			// "\x1b[32m" is green
-			console.log("\x1b[32m%s\x1b[0m", `All ${successful.length}/${unsuccessful.length + successful.length} tests succesful!`);
+			console.log("\x1b[32m%s\x1b[0m", `All ${successful.length}/${unsuccessful.length + successful.length} tests succesful!\n`);
 			return;
 		}
 
-		console.log("\x1b[31m%s\x1b[0m", `\n${successful.length}/${unsuccessful.length + successful.length} tests succesful.`);
+		console.log("\x1b[31m%s\x1b[0m", `\n${successful.length}/${unsuccessful.length + successful.length} tests succesful.\n`);
 
-		let lastCategory = module.exports.any;
+		let lastCategory = ANY;
 
 		for (const res of unsuccessful) {
 			if (res.category != lastCategory) {
