f3cbf976e9a5e7e8f3320c7cb9c86c940749ca1c
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-07-18 07:16:01 +0300

    Add fandom wiki searching

---
+++ text_commands/searchFandom.js
@@ -0,0 +1,40 @@
+const https = require("https");
+
+async function getWebpage(address) {
+	let resHtml = new Promise((resolve, reject) => {
+		https.get(address, (res) => {
+			let totalData = "";
+			if (res.statusCode === 200) {
+				res.on("data", (d) => {
+					totalData += d;
+				})
+					.once("close", () => resolve(totalData));
+			} else {
+				reject(Error(`Request failed with status code: ${res.statusCode}`));
+			}
+		});
+	});
+
+	resHtml = await resHtml;
+
+	return resHtml;
+}
+
+module.exports = {
+	async execute(message, regexResults, extraRegex) {
+
+		const resHtmlWiki = await getWebpage(`https://www.fandom.com/?s=${encodeURIComponent(regexResults[1])}`);
+		const wiki = /<div class="mediawiki-article__content">\s*<a href="https:\/\/([a-zA-Z0-9]+)\.fandom\.com\/wiki/.exec(resHtmlWiki);
+		if (!wiki) {
+			throw Error(`Printable error: I couldn't find a wiki called ${regexResults[1]}.`);
+		}
+
+		const resHtmlArticle = await getWebpage(`https://${wiki[1]}.fandom.com/wiki/Special:Search?query=${encodeURIComponent(regexResults[2])}&scope=internal`);
+		const article = /<h3 class="unified-search__result__header">\s*<a href="([^"]+)"/.exec(resHtmlArticle);
+		if (!article) {
+			throw Error(`Printable error: I couldn't find an article for ${regexResults[2]}`);
+		}
+
+		return { text: article[1] };
+	},
+};
