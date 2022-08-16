936d8241e07556290f9e4829ec1d6c97a0fe4cd5
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-08-16 16:03:02 +0300

    Wrap things in index in a function so they can await

--- src/index.ts
+++ src/index.ts
@@ -17,30 +17,34 @@ const sequelize = new Sequelize("database", "user", "password", {
 	storage: currentStorage,
 });
 
-//	TODO: Use ENUM for reactType instead of INTEGER? Unless, potentially, javascript can't have proper enums?
-client.db = sequelize.define("db", currentCols);
+async function init() {
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync("./commands").filter((file: string) => file.endsWith(".ts"));
-
-for (const file of commandFiles) {
-	const command = await import(`./commands/${file}`);
-	// Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-	client.commands.set(command.data.name, command);
-}
-
-
-const eventFiles = fs.readdirSync("./events").filter((file: string) => file.endsWith(".js"));
-
-for (const file of eventFiles) {
-	const event = await import(`./events/${file}`);
-	if (event.once) {
-		client.once(event.name, (...args: any) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args: any) => event.execute(...args));
+	client.db = sequelize.define("db", currentCols);
+	
+	client.commands = new Collection();
+	const commandFiles = fs.readdirSync("./commands").filter((file: string) => file.endsWith(".ts"));
+	
+	for (const file of commandFiles) {
+		const command = await import(`./commands/${file}`);
+		// Set a new item in the Collection
+		// With the key as the command name and the value as the exported module
+		client.commands.set(command.data.name, command);
 	}
+	
+	
+	const eventFiles = fs.readdirSync("./events").filter((file: string) => file.endsWith(".ts"));
+	
+	for (const file of eventFiles) {
+		const event = await import(`./events/${file}`);
+		if (event.once) {
+			client.once(event.name, (...args: any) => event.execute(...args));
+		} else {
+			client.on(event.name, (...args: any) => event.execute(...args));
+		}
+	}
+	
+	// Login to Discord with your client's token
+	client.login(token);
 }
 
-// Login to Discord with your client's token
-client.login(token);
+init()
