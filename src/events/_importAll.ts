6697b31d597b7fed2770bedc3b7447025b01c9d9
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-08-16 17:35:45 +0300

    Adjust importAll for events

---
+++ src/events/_importAll.ts
@@ -0,0 +1,7 @@
+import * as guildMemberAdd from "./guildMemberAdd"
+import * as guildMemberRemove from "./guildMemberRemove"
+import * as interactionCreate from "./interactionCreate"
+import * as messageCreate from "./messageCreate"
+import * as ready from "./ready"
+
+export { guildMemberAdd, guildMemberRemove, interactionCreate, messageCreate, ready }
