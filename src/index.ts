d906142e5eef5ce9da147474424bdfcedf335bf3
a-One-Fan <100067309+a-One-Fan@users.noreply.github.com>
2022-08-16 12:43:47 +0300

    Convert requires to imports

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 // Require the necessary discord.js classes
-const fs = require("fs");
-const Sequelize = require("sequelize");
-const { Client, Collection, Intents } = require("discord.js");
-const { token } = require("./config.json");
-const { currentStorage, currentCols } = require("./extras/database_stuff");
+import fs from "fs";
+import { Sequelize } from "sequelize";
+import { Client, Collection, Intents } from "discord.js";
+import { token } from "./config.json";
+import { currentStorage, currentCols } from "./extras/database_stuff";
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGES,
+const client: any = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGES,
 	Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.GUILD_MEMBERS,
 	Intents.FLAGS.GUILD_PRESENCES] });
 
@@ -24,7 +24,7 @@ client.commands = new Collection();
 const commandFiles = fs.readdirSync("./commands").filter((file: string) => file.endsWith(".ts"));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+	const command = await import(`./commands/${file}`);
 	// Set a new item in the Collection
 	// With the key as the command name and the value as the exported module
 	client.commands.set(command.data.name, command);
@@ -34,7 +34,7 @@ for (const file of commandFiles) {
 const eventFiles = fs.readdirSync("./events").filter((file: string) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
+	const event = await import(`./events/${file}`);
 	if (event.once) {
 		client.once(event.name, (...args: any) => event.execute(...args));
 	} else {
